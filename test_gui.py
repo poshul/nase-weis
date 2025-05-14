d766c8a4427fad0dc55265c6d244d4bd94904b41
Samuel Wein <sam@samwein.com>
2025-05-14 16:08:48 +0200

    removes tests for removed classes

--- test_gui.py
+++ test_gui.py
@@ -28,9 +28,6 @@ def launch(request):
         "content/topp_workflow_parameter.py",
         "content/topp_workflow_execution.py",
         "content/topp_workflow_results.py",
-        "content/file_upload.py",
-        "content/raw_data_viewer.py",
-        "content/run_example_workflow.py",
         "content/download_section.py",
         "content/simple_workflow.py",
         "content/run_subprocess.py",
@@ -73,62 +70,3 @@ def test_file_upload_load_example(launch):
             i.button[0].click().run()
             assert not launch.exception
 
-
-# NOTE: All tabs are automatically checked
-@pytest.mark.parametrize(
-    "launch,example",
-    [
-        ("content/raw_data_viewer.py", "Blank.mzML"),
-        ("content/raw_data_viewer.py", "Treatment.mzML"),
-        ("content/raw_data_viewer.py", "Pool.mzML"),
-        ("content/raw_data_viewer.py", "Control.mzML"),
-    ],
-    indirect=["launch"],
-)
-def test_view_raw_ms_data(launch, example):
-    launch.run(timeout=30)  # Increased timeout from 10 to 30 seconds
-
-    ## Load Example file, based on implementation of fileupload.load_example_mzML_files() ###
-    mzML_dir = Path(launch.session_state.workspace, "mzML-files")
-
-    # Copy files from example-data/mzML to workspace mzML directory, add to selected files
-    for f in Path("example-data", "mzML").glob("*.mzML"):
-        shutil.copy(f, mzML_dir)
-    launch.run()
-
-    ## TODO: Figure out a way to select a spectrum to be displayed
-    launch.selectbox[0].select(example).run()
-    assert not launch.exception
-
-
-@pytest.mark.parametrize(
-    "launch,example",
-    [
-        ("content/run_example_workflow.py", ["Blank"]),
-        ("content/run_example_workflow.py", ["Treatment"]),
-        ("content/run_example_workflow.py", ["Pool"]),
-        ("content/run_example_workflow.py", ["Control"]),
-        ("content/run_example_workflow.py", ["Control", "Blank"]),
-    ],
-    indirect=["launch"],
-)
-def test_run_workflow(launch, example):
-    launch.run()
-    ## Load Example file, based on implementation of fileupload.load_example_mzML_files() ###
-    mzML_dir = Path(launch.session_state.workspace, "mzML-files")
-
-    # Copy files from example-data/mzML to workspace mzML directory, add to selected files
-    for f in Path("example-data", "mzML").glob("*.mzML"):
-        shutil.copy(f, mzML_dir)
-    launch.run()
-
-    ## Select experiments to process
-    for e in example:
-        launch.multiselect[0].select(e)
-
-    launch.run()
-    assert not launch.exception
-
-    # Press the "Run Workflow" button
-    launch.button[1].click().run(timeout=60)
-    assert not launch.exception
